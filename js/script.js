745d6e809b3204397dc74fdf0ef981473a7165a0
caseymm <caseymm@gmail.com>
2015-01-18 12:58:37 -0500

    added link to style

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
 window.onload = function() {
 
-    var labelTestData = [
-{label: "FOX", times: [{"starting_time": 1355752800000, "ending_time": 1355759900000, "thumb": "Bowser.gif"}, {"starting_time": 1355767900000, "ending_time": 1355774400000, "thumb": "Bowser.gif"}]},
-{label: "MSNBC", times: [{"starting_time": 1355752800000, "ending_time": 1355759900000, "thumb": "Bowser.gif"}, {"starting_time": 1355759910000, "ending_time": 1355761900000, "thumb": "Bowser.gif"}, ]},
-{label: "VOX", times: [{"starting_time": 1355761910000, "ending_time": 1355763910000, "thumb": "Bowser.gif"}]},
-];
-
-var labelColorTestData = [
-{label: "FOX", times: [{"color":"green", "label":"Weeee", "starting_time": 1355752800000, "ending_time": 1355759900000}, {"color":"blue", "label":"Weeee", "starting_time": 1355767900000, "ending_time": 1355774400000}]},
-{label: "MSNBC", times: [{"color":"pink", "label":"Weeee", "starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
-{label: "VOX", times: [{"color":"yellow", "label":"Weeee", "starting_time": 1355761910000, "ending_time": 1355763910000}]},
-];
-
-
 var width = 1000;
 
 function timelineHover() {
@@ -27,7 +14,7 @@ function timelineHover() {
 
         var chart = d3.timeline()
         .stack()
-        .margin({left:70, right:30, top:0, bottom:0})
+        .margin({left:150, right:30, top:0, bottom:0})
         .hover(function (d, i, datum) {
             var div = $('#hoverRes');
             var colors = chart.colors();
